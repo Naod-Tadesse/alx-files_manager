19dc9e0c0fa2e8776a0420bc78853224f6b0e01e
Naod Taddese <naodtds@gmail.com>
2024-08-07 20:25:23 +0300

    rate limit

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,8 +1,8 @@
 import express from 'express';
 
 /**
- * Adds middlewares to the given express application.
- * @param {express.Express} api The express application.
+ * add middle ware with file limit.
+ * @param {express.Express} express api
  */
 const injectMiddlewares = (api) => {
   api.use(express.json({ limit: '200mb' }));
