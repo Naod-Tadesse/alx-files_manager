c56c664c99000d675ba6302084b825a381fe1ef1
Naod Taddese <naodtds@gmail.com>
2024-08-07 20:21:48 +0300

    authentication handler

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,6 +2,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import redisClient from '../utils/redis';
 
+
 export default class AuthController {
   static async getConnect(req, res) {
     const { user } = req;
